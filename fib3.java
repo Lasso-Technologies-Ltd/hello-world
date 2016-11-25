1634fb7493dd290f61824e55ddc464861ea461a9
Lasso-Technologies-Ltd <dgichuhi@grouplasso.com>
2016-11-25 11:02:38 +0300

    Fibonacci with 1 1 1 3 5 9 sequence
    Using BigInteger in Java

--- fib3.java
+++ fib3.java
@@ -1,1 +1,23 @@
 
+import java.math.BigInteger;
+
+
+public class Fib {
+public static void main(String[] args) {
+        int FibonacciCount = 4000;
+        BigInteger fibonacci [] = new BigInteger[FibonacciCount];
+        //declare object called fibonacci [] as an array 
+        fibonacci[0]=BigInteger.ONE;
+        fibonacci[1]=BigInteger.ONE;
+        fibonacci[2]=BigInteger.ONE;
+        //initialize the fibonacci object to 1 using the BigInteger constant ONE
+        
+        for(int j = 3;j<FibonacciCount;j++){
+        fibonacci[j]= fibonacci[j-1].add(fibonacci[j-2]).add(fibonacci[j-3]);
+            
+        }
+        for(int i= 0;i<FibonacciCount;i++){
+        System.out.println(fibonacci[i]+"");
+        }
+    }
+}
